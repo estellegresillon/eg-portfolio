cef33a6129fd3d99a240d81fccf6184fae467959
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-29 10:26:36 +0100

    :tada: first commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      Hello, World
+    </header>
+  </div>
+);
+
+export default App;
