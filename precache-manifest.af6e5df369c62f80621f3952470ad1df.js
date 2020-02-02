ac0885f58ef5df0944c5921d56e47704cddd7183
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-02-02 11:03:36 +0100

    Updates

---
+++ precache-manifest.af6e5df369c62f80621f3952470ad1df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3cc1b3d52810f2daa744fea3514f30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5d185c1e01b177cddc5",
+    "url": "/static/css/main.5d086c45.chunk.css"
+  },
+  {
+    "revision": "2bbf7cfdb68df805af04",
+    "url": "/static/js/2.34eabe07.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.34eabe07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5d185c1e01b177cddc5",
+    "url": "/static/js/main.a9b07950.chunk.js"
+  },
+  {
+    "revision": "757d0ab73d2ff1d9eede",
+    "url": "/static/js/runtime-main.88404f62.js"
+  },
+  {
+    "revision": "ecb0c2ae369ba2a89d9a1ec2a1b3187b",
+    "url": "/static/media/AvenirLTStd-Book.ecb0c2ae.otf"
+  }
+]);
