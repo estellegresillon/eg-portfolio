f1ef8d001663a315e935f83e4358af5723eaafb3
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-02-07 21:47:19 +0100

    updated foodlab project

--- src/components/projects/constants.js
+++ src/components/projects/constants.js
@@ -1,7 +1,7 @@
 export const PROJECTS = [
   { name: "Rafael Bolano", year: 2020, img: "rafaelbolano", url: "http://rafaelbolano.estellegresillon.fr" },
   { name: "Zest", year: 2020, img: "zest", url: "http://zest.estellegresillon.fr" },
-  { name: "Foodlab", year: 2019, img: "foodlab", url: "http://foodlab.estellegresillon.fr" },
+  { name: "Foodlab", year: 2019, img: "foodlab", url: "https://foodlab.space" },
   { name: "Dive In Santorini", year: 2019, img: "santorini", url: "http://santorini.estellegresillon.fr" },
   { name: "Otaku Market", year: 2018, img: "otaku", url: "https://www.otaku-market.com" },
   { name: "That's My Rock", year: 2018, img: "thatsmyrock", url: "http://www.thatsmyrock.com/" },
