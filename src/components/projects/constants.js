8fd6e0b1d1104e142a9acf1f1604e812b9a55859
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-02-08 10:32:41 +0100

    updated footer and lang

--- src/components/projects/constants.js
+++ src/components/projects/constants.js
@@ -1,8 +1,8 @@
 export const PROJECTS = [
-  { name: "Rafael Bolano", year: 2020, img: "rafaelbolano", url: "http://rafaelbolano.estellegresillon.fr" },
-  { name: "Zest", year: 2020, img: "zest", url: "http://zest.estellegresillon.fr" },
+  { name: "Rafael Bolano", year: 2020, img: "rafaelbolano", url: "https://rafaelbolano.estellegresillon.fr" },
+  { name: "Zest", year: 2020, img: "zest", url: "https://zest.estellegresillon.fr" },
   { name: "Foodlab", year: 2019, img: "foodlab", url: "https://foodlab.space" },
-  { name: "Dive In Santorini", year: 2019, img: "santorini", url: "http://santorini.estellegresillon.fr" },
+  { name: "Dive In Santorini", year: 2019, img: "santorini", url: "https://santorini.estellegresillon.fr" },
   { name: "Otaku Market", year: 2018, img: "otaku", url: "https://www.otaku-market.com" },
   { name: "That's My Rock", year: 2018, img: "thatsmyrock", url: "http://www.thatsmyrock.com/" },
 ];
