75f1daefa752aeb9c739af2ccb12d066856d96d9
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-02-10 09:52:43 +0100

    updated react-snap installation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, combineReducers } from "redux";
 import { I18nextProvider } from 'react-i18next';
@@ -30,11 +30,24 @@ const App = () => (
   </BrowserRouter>
 );
 
-ReactDOM.render(
-  <I18nextProvider i18n={i18n}>
-    <Provider store={createStore(reducers)}>
-      <App />
-    </Provider>
-  </I18nextProvider>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <I18nextProvider i18n={i18n}>
+      <Provider store={createStore(reducers)}>
+        <App />
+      </Provider>
+    </I18nextProvider>,
+    rootElement
+  );
+} else {
+  render(
+    <I18nextProvider i18n={i18n}>
+      <Provider store={createStore(reducers)}>
+        <App />
+      </Provider>
+    </I18nextProvider>,
+    rootElement
+  );
+}
